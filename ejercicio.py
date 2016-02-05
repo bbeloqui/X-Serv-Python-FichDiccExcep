c125d996bd5abd4bf78c87a42e033d6342649bc5
bbeloqui <bbeloqui@hotmail.com>
2016-02-05 18:14:40 +0100

    ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# hola, hola
-
 fd = open('/etc/passwd', 'r')
 
 lineas = fd.readlines()
 fd.close()
-
+dict = {} 
 for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+    elementos = linea.split(":")
+   #print elementos[0], elementos[-1][:-1]   no nos pide que las imprimamos
+    dict[elementos[0]] = elementos[-1][:-1] 
+
+print "La shell de root es:", dict['root']
+try:  
+    print"La shell de imaginario es:", dict['imaginario']
+except (KeyError):
+	print "no existe la shell de imaginario"
+
+
